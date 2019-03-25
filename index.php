c73c905a47fd47a1b106ec88fc457b496e92d72c
Kayle <postvoorkayle@hotmail.com>
2019-03-25 15:24:14 +0100

    Index page af

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php require 'header.php'; ?>
     <link rel="stylesheet" type="text/css" href="index.css">
 
-
+<div class="grid">
    <h1>A day at the races</h1>
    <p>You'll need to ether login or register to be able to download the file.</p>
-    <a class="loginButton" href='login.php'>Login</a>
-    <a class="registerButton" href='register.php'>register</a>
-    <img src="imgs/video.png" alt="Video of dogs">
-
+<div>
+        <a class="loginButton" href='login.php'><img src="imgs/Login.png" alt=""></a>
+    <a class="registerLink" href='register.php'>register</a>
+</div>
+    <img class="dog" src="imgs/video.png" alt="Video of dogs">
+</div>
 
 <?php require 'footer.php'; ?>
