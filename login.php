be25bd23b82bd4f7c0bb64c02f8f2744836dd3c6
Kayle <postvoorkayle@hotmail.com>
2019-03-25 16:31:17 +0100

    Login form af

--- login.php
+++ login.php
@@ -1,20 +1,18 @@
 <?php require 'header.php'; ?>
-
+<link rel="stylesheet" type="text/css" href="login.css">
 <h1 class="loginh1">login om te downloaden</h1>
 
 <form class="loginform" action="includes/logincontroller.php" method="post">
     <input type="hidden" name="type" value="login">
     <div class="form-group">
-        <label for="email">Email</label>
-        <input type="text" name="email" id="email">
+        <input class="form" type="text" name="email" id="email">
     </div>
 
     <div class="form-group">
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password">
+        <input class="form" type="password" name="password" id="password">
     </div>
 
-    <input type="submit" value="login">
+    <input class="formbutton" type="image" src="imgs/Login.png" value="login">
 </form>
 
 <?php require 'footer.php'; ?>
